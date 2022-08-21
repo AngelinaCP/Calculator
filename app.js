cfff89ec81837d42534169039d8c4bb37975ceed
Dwemer Delena <ddelena@pr-a2.kzn.21-school.ru>
2022-08-21 17:02:49 +0300

    working version

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
-let input = '';
-let sign = '';
-let num = 0;
-let firstNum = 0;
-let secondNum = 0;
-let sum = 0;
-let numString = ''
-function calculate( firstNum, secondNum, sign) {
+function calculate(firstNum, secondNum, sign) {
+  console.log(firstNum, secondNum, sign);
     if (sign === '+') {
       return parseInt(firstNum) + parseInt(secondNum);
-    }
+    } else if (sign === '-') {
+      return parseInt(firstNum) - parseInt(secondNum);
+    } else if (sign === '*') {
+      return parseInt(firstNum) * parseInt(secondNum);
+    } else if (sign === '/') {
+      return parseInt(firstNum) / parseInt(secondNum);
+    } 
 }
-buttons.forEach((item) => {
+
+function checkSign(sign) {
+  switch (sign) {
+    case '+':
+      return '+';
+    case '-':
+      return '-';
+    case '%':
+      return '%';
+    case '/':
+      return '/';
+    case '*':
+      return '*';
+  }
+  return '';
+}
+
+function main() {
+  let input = '';
+  let sign = '';
+  let num = 0;
+  let firstNum = ''
+  let secondNum ='';
+  let sum = -1;
+  let numString = ''
+  const action = ['-', '*', '/', '+'];
+
+  buttons.forEach((item) => {
     item.onclick = () => {
-      //here we have + - % / * 
-      if (item.id == "clear") {
-        input = ''
-        num = 0;
-       numString = ''
-        display.innerText = "";
-      } 
-      else if (item.id  == '+') {
-        firstNum = parseInt(numString);
-        console.log('this is firsNum', firstNum);
-        sign = '+'
+      // console.log('this is a sign',);
+      if (item.id === 'clear') {
+        display.innerText = '';
+        sum = 0;
+      } else if (action.includes(item.id)) {
+        if (sum === -1) {
+          sum = parseInt(numString);
+          numString = ''
+        }
+        sign = checkSign(item.id)
+      } else if (!isNaN(item.id)){
+        display.innerText += item.id;
+        numString += item.id;
       }
-      else if (item.id == 'equal') {
-        calculate(sign, firstNum, secondNum);
-      } 
-      else if (typeof (parseInt(item.id)) === 'Number') {
-        console.log('typeof (item.id)');
-      } 
-      else {
-          display.innerText += item.id;
-          numString += item.id
-          // console.log('firstnum', firstNum, 'secondNum', num);
-          if (sign) {
-            num = parseInt(numString);
-            num = calculate(firstNum, num, sign)
-            display.innerText = num
-            sign = ''
-          }
+      else if ((sign && item.id === 'equal' && numString)) {
+        sum = calculate(sum, parseInt(numString), sign);
+        // console.log('sum', sum);
+        numString = ''
+        display.innerText = sum;
       }
-      // else if (item.id == '+') {
-      //   display.innerText = num
-      // } else {
-      //   display.innerText += item.id;
-      // }
-    // if (item.id == "clear") {
-    //     input = ''
-    //     num = 0;
-    //     display.innerText = "";
-    // } else if (display.innerText != "" && item.id === "+") {
-    //     num += parseInt(item.id);
-    //     console.log('this is num', num);
-    // } else if (display.innerText == "" && item.id == "equal") {
-    //     display.innerText = "Empty!";
-    //     setTimeout(() => (display.innerText = ""), 2000);
-    // } else if (item.id == "equal") {
-    //   // console.log('num', num);
-    //   display.innerText = num;
-    // } else {
-    //     input = item.id;
-    //     num += input;
-    //     display.innerText = item.id;
-    // }
   };
 });
+}
 
-// const themeToggleBtn = document.querySelector(".theme-toggler");
-// const calculator = document.querySelector(".calculator");
-// const toggleIcon = document.querySelector(".toggler-icon");
-// let isDark = true;
-// themeToggleBtn.onclick = () => {
-//   calculator.classList.toggle("dark");
-//   themeToggleBtn.classList.toggle("active");
-//   isDark = !isDark;
-// };
+main();
+
+const themeToggleBtn = document.querySelector(".theme-toggler");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggler-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
