1a7d6a31f2f7ac5145b0b57930d02f90320140da
Dwemer Delena <ddelena@va-g4.kzn.21-school.ru>
2022-08-21 12:29:26 +0300

    some changes

--- app.js
+++ app.js
@@ -2,25 +2,71 @@ const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
 
 let input = '';
-
+let sign = '';
+let num = 0;
+let firstNum = 0;
+let secondNum = 0;
+let sum = 0;
+let numString = ''
+function calculate( firstNum, secondNum, sign) {
+    if (sign === '+') {
+      return parseInt(firstNum) + parseInt(secondNum);
+    }
+}
 buttons.forEach((item) => {
-    let num = 0;
-  item.onclick = () => {
-    if (item.id == "clear") {
+    item.onclick = () => {
+      //here we have + - % / * 
+      if (item.id == "clear") {
         input = ''
+        num = 0;
+       numString = ''
         display.innerText = "";
-    } else if (display.innerText != "" && item.id === "+") {
-        num = parseInt(input)   
-        console.log(input);
-    }
-    else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } 
-    else {
-        input += item.id;
-        display.innerText += item.id;
-    }
+      } 
+      else if (item.id  == '+') {
+        firstNum = parseInt(numString);
+        console.log('this is firsNum', firstNum);
+        sign = '+'
+      }
+      else if (item.id == 'equal') {
+        calculate(sign, firstNum, secondNum);
+      } 
+      else if (typeof (parseInt(item.id)) === 'Number') {
+        console.log('typeof (item.id)');
+      } 
+      else {
+          display.innerText += item.id;
+          numString += item.id
+          // console.log('firstnum', firstNum, 'secondNum', num);
+          if (sign) {
+            num = parseInt(numString);
+            num = calculate(firstNum, num, sign)
+            display.innerText = num
+            sign = ''
+          }
+      }
+      // else if (item.id == '+') {
+      //   display.innerText = num
+      // } else {
+      //   display.innerText += item.id;
+      // }
+    // if (item.id == "clear") {
+    //     input = ''
+    //     num = 0;
+    //     display.innerText = "";
+    // } else if (display.innerText != "" && item.id === "+") {
+    //     num += parseInt(item.id);
+    //     console.log('this is num', num);
+    // } else if (display.innerText == "" && item.id == "equal") {
+    //     display.innerText = "Empty!";
+    //     setTimeout(() => (display.innerText = ""), 2000);
+    // } else if (item.id == "equal") {
+    //   // console.log('num', num);
+    //   display.innerText = num;
+    // } else {
+    //     input = item.id;
+    //     num += input;
+    //     display.innerText = item.id;
+    // }
   };
 });
 
